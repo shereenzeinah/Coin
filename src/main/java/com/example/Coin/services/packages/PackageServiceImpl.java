1b236e438d2f5f06ba709da9769c4ba54dda9cd4
Shereen Zeinah <shereen.zeinah@gmail.com>
2023-07-01 13:38:45 +0300

    Added transactions to packages

--- src/main/java/com/example/Coin/services/packages/PackageServiceImpl.java
+++ src/main/java/com/example/Coin/services/packages/PackageServiceImpl.java
@@ -2,11 +2,15 @@ package com.example.Coin.services.packages;
 
 import com.example.Coin.models.Package;
 import com.example.Coin.models.PackageDTO;
-
+import com.example.Coin.models.Transactions;
 import com.example.Coin.repository.PackageRepository;
+import com.example.Coin.repository.TransactionsRepository;
+
 import org.springframework.stereotype.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -14,6 +18,10 @@ public class PackageServiceImpl implements PackageService {
 
     @Autowired
     PackageRepository repository;
+
+    
+    @Autowired
+    TransactionsRepository transactionsRepository;
 
     @Override
     public List<Package> getAll() {
@@ -24,7 +32,13 @@ public class PackageServiceImpl implements PackageService {
     public Package createPackage(PackageDTO packageDTO) {
         Package packageObj = new Package(packageDTO.name, packageDTO.description, packageDTO.coins);
         repository.save(packageObj);
-
+        Transactions transactions = Transactions.builder()
+                .transactionType("PACKAGE-ADD")
+                .transactionDate(new Date())
+                .description("Package with id: " + packageObj.getId() +" has been added")
+                .build();
+        transactionsRepository.save(transactions);
+        
         return packageObj;
     }
 
@@ -32,12 +46,24 @@ public class PackageServiceImpl implements PackageService {
     public Package editPackage(PackageDTO packageDTO) {
         Package packageObj = new Package(packageDTO.id, packageDTO.name, packageDTO.description, packageDTO.coins);
         repository.save(packageObj);
-
+        Transactions transactions = Transactions.builder()
+                .transactionType("PACKAGE-UPDATE")
+                .transactionDate(new Date())
+                .description("Package with id: " + packageDTO.id +" has been updated")
+                .build();
+        transactionsRepository.save(transactions);
         return packageObj;
     }
 
     @Override
     public void deletePackage(String id) {
         repository.deleteById(id);
+        //Update transactions
+        Transactions transactions = Transactions.builder()
+                .transactionType("PACKAGE-DELETED")
+                .transactionDate(new Date())
+                .description("Package with id: " +id +" has been deleted")
+                .build();
+        transactionsRepository.save(transactions);
     }
 }
